e010b6591f420cdd97019ad72249526dbef38c27
tkdalsgks <hsm_01@naver.com>
2022-04-23 16:57:52 +0900

    chore : 레시피 상세페이지 DB에 저장된 사진 출력 - 상민

---
+++ src/main/java/com/recipe/gola/dto/RecipeLinkDTO.java
@@ -0,0 +1,21 @@
+package com.recipe.gola.dto;
+
+import lombok.Data;
+
+@Data
+public class RecipeLinkDTO {
+
+	private String no;	// 번호
+	private String menuImg;	// 메뉴 이미지
+	private String link1;	// 레시피1 이미지
+	private String link2;	// 레시피2 이미지
+	private String link3;	// 레시피3 이미지
+	private String link4;	// 레시피4 이미지
+	private String link5;	// 레시피5 이미지
+	private String link6;	// 레시피6 이미지
+	private String link7;	// 레시피7 이미지
+	private String link8;	// 레시피8 이미지
+	private String link9;	// 레시피9 이미지
+	private String link10;	// 레시피10 이미지
+	
+}
