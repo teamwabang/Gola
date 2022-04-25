6a607c23d4bf0428ee1f2137c880706ee8b43156
tkdalsgks <hsm_01@naver.com>
2022-04-25 17:30:19 +0900

    style : html, css, js 분리 - 상민

--- src/main/resources/static/js/datatable.js
+++ src/main/resources/static/js/datatable.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-	$('#recipe').DataTable({
+	$('#userlist').DataTable({
 		"dataSrc": "",
 		"bLengthChange": false,
 		"pageLength": 10,
 		"language": {
-			"emptyTable": "게시글이 없습니다.",
+			"emptyTable": "유저가 없습니다.",
 			"info": "총 _TOTAL_건",
-			"search": "레시피 검색 : ",
+			"search": "회원 검색 : ",
 			"zeroRecords": "일치하는 데이터가 없습니다.",
 			"paginate": {
 				"first": "처음",
@@ -19,14 +19,14 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-	$('#userlist').DataTable({
+	$('#bbs_list').DataTable({
 		"dataSrc": "",
 		"bLengthChange": false,
 		"pageLength": 10,
 		"language": {
-			"emptyTable": "유저가 없습니다.",
+			"emptyTable": "게시글이 없습니다.",
 			"info": "총 _TOTAL_건",
-			"search": "회원 검색 : ",
+			"search": "검색 : ",
 			"zeroRecords": "일치하는 데이터가 없습니다.",
 			"paginate": {
 				"first": "처음",
