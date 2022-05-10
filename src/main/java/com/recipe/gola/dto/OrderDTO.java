a93330f07ded60b7a751b8e4dd675b8010bef1e1
tkdalsgks <hsm_01@naver.com>
2022-05-10 11:44:19 +0900

    chore : mybatis 로컬 실행 임시 변경 - 상민

--- src/main/java/com/recipe/gola/dto/OrderDTO.java
+++ src/main/java/com/recipe/gola/dto/OrderDTO.java
@@ -1,5 +1,8 @@
 package com.recipe.gola.dto;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import lombok.Data;
 
 @Data
@@ -14,5 +17,5 @@ public class OrderDTO {
 	private String oAddr1;	// 수령자 주소
 	private String oAddr2;	// 수령자 상세주소
 	private String oRequest;	// 배송 시 요청사항
-	private String oRegdate;	// 주문 날짜
+	private String oRegdate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));	// 주문 날짜
 }
