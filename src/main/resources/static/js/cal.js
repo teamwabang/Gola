9722878f10cec0e57191ed44b1264234c3a3826d
bohongu <jaehong2782@naver.com>
2022-05-11 13:26:06 +0900

    chore : cal 수정 - 재홍

--- src/main/resources/static/js/cal.js
+++ src/main/resources/static/js/cal.js
@@ -4,34 +4,58 @@ let ageValue = document.getElementById("age");
 
 let calResult = document.getElementById("calResult");
 
-function getResult(event) {
+function getResult() {
   event.preventDefault();
 
   if (!document.querySelector("input[name='gender']:checked")) {
-    alert("성별체크");
+    swal({
+      title: "성별을 선택해주세요",
+      icon: "info",
+      button: "확인",
+    });
+    return false;
   }
 
   if (height.value == "") {
-    alert("키 입력");
+    swal({
+      title: "키(cm)를 입력해주세요",
+      icon: "info",
+      button: "확인",
+    });
   }
 
   if (weight.value == "") {
-    alert("몸무게 입력");
+    swal({
+      title: "몸무게(kg)를 입력해주세요",
+      icon: "info",
+      button: "확인",
+    });
   }
 
   if (!document.querySelector("input[name='level']:checked")) {
-    alert("활동레벨체크");
+    swal({
+      title: "활동레벨을 선택해주세요",
+      icon: "info",
+      button: "확인",
+    });
   }
 
-  calResult.innerHTML = `✔ 당신의 하루 권장 칼로리는 ${Math.round(
-    10 * weight.value +
-      6.25 * height.value -
-      5.0 * ageValue.options[ageValue.selectedIndex].value +
-      Number(document.querySelector("input[name='gender']:checked").value) *
+  if (
+    weight.value != "" &&
+    height.value != "" &&
+    document.querySelector("input[name='gender']:checked").value &&
+    document.querySelector("input[name='level']:checked").value
+  ) {
+    calResult.innerHTML = `🥑 당신의 하루 권장 칼로리는 ${Math.round(
+      (10 * weight.value +
+        6.25 * height.value -
+        5.0 * ageValue.options[ageValue.selectedIndex].value +
+        Number(document.querySelector("input[name='gender']:checked").value)) *
         document.querySelector("input[name='level']:checked").value
-  )}Kcal 입니다.`;
+    )}Kcal 입니다.`;
+  }
 }
 
 calResult.style.fontWeight = "bold";
 calResult.style.fontSize = "17px";
-calResult.style.marginBottom = "3%";
+calResult.style.color = "green";
