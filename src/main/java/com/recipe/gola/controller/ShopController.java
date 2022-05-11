36a1d651a8891970df9a56a6724f7b488acee129
yuuulya <yla101@naver.com>
2022-05-11 17:45:53 +0900

    feat : shopdetail html, css controller 추가

---
+++ src/main/java/com/recipe/gola/controller/ShopController.java
@@ -0,0 +1,34 @@
+package com.recipe.gola.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.Data;
+
+@Controller
+@Data
+public class ShopController {
+	
+	@Value("${file.upload.directory}")
+	private String fileUploadPath;
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	// 쇼핑몰 페이지 
+	@GetMapping("shop")
+	public String shop_index() {
+		logger.info("-----> 쇼핑몰 페이지로 이동합니다.");
+		return "shop/shop";
+	}
+	
+	// 쇼핑몰 상세페이지 
+	@GetMapping("shopdetail")
+	public String shop_detail() {
+		logger.info("-----> 쇼핑몰 상세 페이지로 이동합니다.");
+		return "shop/shopdetail";
+	}
+	
+}
