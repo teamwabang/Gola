066047afa465a098d0e4412da8fb32b71231d0e5
tkdalsgks <hsm_01@naver.com>
2022-05-13 17:50:41 +0900

    chore : cart, order 컨트롤러 분리, 주소값 변경 - 상민

--- src/main/java/com/recipe/gola/controller/OrderController.java
+++ src/main/java/com/recipe/gola/controller/OrderController.java
@@ -1,16 +1,13 @@
 package com.recipe.gola.controller;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.recipe.gola.dto.ProductDTO;
 import com.recipe.gola.service.RecipeLinkService;
 import com.recipe.gola.service.ShopService;
 
@@ -18,7 +15,8 @@ import lombok.Data;
 
 @Controller
 @Data
-public class ShopController {
+@RequestMapping("shop")
+public class OrderController {
 	
 	@Value("${file.upload.directory}")
 	private String fileUploadPath;
@@ -31,21 +29,18 @@ public class ShopController {
 	@Autowired
 	private final RecipeLinkService recipelinkService;
 	
-	
-	// 쇼핑몰 페이지 
-	@GetMapping("shop")
-	public String shoplist(Model model, ProductDTO dto) {
-		logger.info("-----> 쇼핑몰 페이지로 이동하여 전체 리스트를 출력합니다.");
-		List<ProductDTO> shoplist = ShopService.shoplist(dto);
-		model.addAttribute("shoplist", shoplist);
-		return "shop/shop";
+	// 주문서작성/결제 
+	@GetMapping("order")
+	public String shop_order() {
+		logger.info("-----> 주문서작성/결제 페이지로 이동합니다.");
+		return "shop/order";
 	}
-
-	// 쇼핑몰 상세페이지 
-	@GetMapping("shop/detail")
-	public String shop_detail() {
-		logger.info("-----> 쇼핑몰 상세 페이지로 이동합니다.");
-		return "shop/shopdetail";
+	
+	// 주문완료
+	@GetMapping("order/result")
+	public String shop_order_result() {
+		logger.info("-----> 주문이 정상적으로 완료되었습니다.");
+		return "shop/order_result";
 	}
 
 }
