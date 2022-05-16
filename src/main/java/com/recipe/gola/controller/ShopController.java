92455f55aad709c68b6937d082308c257003961b
tkdalsgks <hsm_01@naver.com>
2022-05-16 15:26:29 +0900

    chore : 쇼핑몰 상세페이지 DB에 저장된 값 출력 - 상민

--- src/main/java/com/recipe/gola/controller/ShopController.java
+++ src/main/java/com/recipe/gola/controller/ShopController.java
@@ -11,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.recipe.gola.dto.ProductDTO;
-import com.recipe.gola.service.RecipeLinkService;
 import com.recipe.gola.service.ShopService;
 
 import lombok.Data;
@@ -26,25 +25,24 @@ public class ShopController {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
-	private final ShopService ShopService;
-	
-	@Autowired
-	private final RecipeLinkService recipelinkService;
+	private final ShopService shopService;
 	
 	
 	// 쇼핑몰 페이지 
 	@GetMapping("shop")
 	public String shoplist(Model model, ProductDTO dto) {
 		logger.info("-----> 쇼핑몰 페이지로 이동하여 전체 리스트를 출력합니다.");
-		List<ProductDTO> shoplist = ShopService.shoplist(dto);
+		List<ProductDTO> shoplist = shopService.shoplist(dto);
 		model.addAttribute("shoplist", shoplist);
 		return "shop/shop";
 	}
 
 	// 쇼핑몰 상세페이지 
 	@GetMapping("shop/detail")
-	public String shop_detail() {
-		logger.info("-----> 쇼핑몰 상세 페이지로 이동합니다.");
+	public String shopdetail(ProductDTO dto, Model model) {
+		logger.info("-----> " + dto.getPno() + "번 재료를 출력합니다.");
+		dto = shopService.shopdetail(dto);
+		model.addAttribute("shopdetail", dto);
 		return "shop/shopdetail";
 	}
 
