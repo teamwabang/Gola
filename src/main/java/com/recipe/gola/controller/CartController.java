ce17a8a649b8119677e55ad10f1370f7899b0dc0
tkdalsgks <hsm_01@naver.com>
2022-05-17 23:17:03 +0900

    chore : 장바구니에 동일한 상품한 상품이 있는지 확인 여부 추가 - 상민

--- src/main/java/com/recipe/gola/controller/CartController.java
+++ src/main/java/com/recipe/gola/controller/CartController.java
@@ -1,7 +1,9 @@
 package com.recipe.gola.controller;
 
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.recipe.gola.config.auth.PrincipalDetails;
 import com.recipe.gola.dto.CartDTO;
@@ -38,7 +41,7 @@ public class CartController {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
-	private final ShopService ShopService;
+	private final ShopService shopService;
 	
 	@Autowired
 	private final CartService cartService;
@@ -51,13 +54,23 @@ public class CartController {
 	
 	// 장바구니
 	@GetMapping("cart")
-	public void shop_cart(@AuthenticationPrincipal PrincipalDetails principaldetail, Model model) {
+	public ModelAndView shop_cart(@AuthenticationPrincipal PrincipalDetails principaldetail, ModelAndView mv) {
 		logger.info("-----> 장바구니 페이지로 이동합니다.");
     	
 		String userId = principaldetail.getUsername();
 		
+		Map<String, Object> map = new HashMap<String, Object>();
 		List<CartDTO> list = cartService.list(userId);
-		model.addAttribute("cartlist", list);
+		int sumMoney = cartService.sumMoney(userId);
+		int fee = sumMoney >= 30000 ? 0 : 3000;
+		map.put("list", list);	// 장바구니 정보를 map에 저장
+		map.put("count", list.size());	// 장바구니 상품 유무
+		map.put("sumMoney", sumMoney);	// 장바구니 전체 금액
+		map.put("fee", fee);	// 배송료
+		map.put("allSum", sumMoney + fee);	// 주문 상품 전체 금액 (상품 + 배송료)
+		mv.setViewName("shop/cart");
+		mv.addObject("map", map);
+		return mv;
 	}
 	
 	// 장바구니에 물건 담기
@@ -73,15 +86,15 @@ public class CartController {
     	int pno = productdto.getPno();
     	cartdto.setPno(pno);
     	
-    	if(userId == null) {
-    		out.println("<script>alert('비회원은 장바구니에 담을 수 없습니다. 로그인 해주세요.'); location.href='/shop';</script>");
-            out.flush();
-    		return "redirect:/shop";
+    	int count = cartService.countCart(cartdto.getPno(), userId);
+    	if(count == 0) {
+    		// 장바구니에 동일한 상품이 없으면
+    		cartService.insert(cartdto);
     	} else {
-    		cartService.insert(cartdto);    		
-    		return "redirect:/shop/cart";
+    		// 장바구니에 동일한 상품이 있으면
+    		cartService.update(cartdto);
     	}
-    	
+    	return "redirect:/shop";
 	}
 
 }
