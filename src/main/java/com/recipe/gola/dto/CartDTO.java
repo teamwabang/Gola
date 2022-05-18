6bcb60570192e785402f22594580283044d2e19f
tkdalsgks <hsm_01@naver.com>
2022-05-18 14:00:18 +0900

    chore : 내가 담은 장바구니 목록 조회 오류 수정 - 율리아, 상민

--- src/main/java/com/recipe/gola/dto/CartDTO.java
+++ src/main/java/com/recipe/gola/dto/CartDTO.java
@@ -8,5 +8,6 @@ public class CartDTO {
 	private int cno;	// 장바구니 번호
 	private String userId;	// 회원 아이디
 	private int pno;	// 상품 번호
-	private int cQuantity;	// 상품 수량
+	private int camount;	// 상품 수량
+	private ProductDTO ProductDTO;
 }
